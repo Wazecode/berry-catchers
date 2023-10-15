751d1907792a4a55ce0a70f85294e73696e35102
Shuwais <shuwais2003@protonmail.com>
2023-10-16 02:02:06 +0530

    version 1 completed

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ canvas.height = window.innerHeight;
 
 //RENDER SETTINGS
 var context = canvas.getContext('2d');
-context.font = "30px Comic Sans MS";
+context.font = "50px Comic Sans MS";
 context.textAlign = "center";
+
+// Images
+const IMAGE_BC = document.getElementById('Berry-Catchers');
+const IMAGE_BASKET = document.getElementById('basket');
+const IMAGE_CHERRY = document.getElementById('cherry');
 
 // Games States
 const GAME_RUNNING = 1;
@@ -19,11 +24,11 @@ const DIFF_CHANGE = 3;
 var gameState = GAME_START;
 
 // Player's stuff
-const PLAYER_VEL = 10;
+const PLAYER_VEL = 20;
 const PLAYER_INTIAL_POS_X = x = canvas.width / 2;
-const PLAYER_INTIAL_POS_Y = 9 * canvas.height / 10;
-const PLAYER_WIDTH = 90;
-const PLAYER_HEIGTH = 10;
+const PLAYER_INTIAL_POS_Y = 8 * canvas.height / 10;
+const PLAYER_WIDTH = 150;
+const PLAYER_HEIGTH = 90;
 
 var pos = PLAYER_INTIAL_POS_X
 
@@ -31,22 +36,22 @@ var score = 0;
 var difficulty = 1;
 
 // Cherry's stuff 
+const CHERRY_SIZE = 70;
+const CHERRY_MAX_SPEED = 7;
+const CHERRY_MIN_SPEED = 2;
 class Cherry {
-    static color = 'red';
-    static CHERRY_SIZE = 20;
     constructor() {
         this.reset();
     }
 
     reset() {
         this.posX = getRandomInt(0, canvas.width);
-        this.dropSpeed = getRandomInt(1,5);
+        this.dropSpeed = getRandomInt(3,8);
         this.posY = -10;
     }
 
     draw() {
-        context.fillStyle = 'red';
-        context.fillRect(this.posX, this.posY , 30, 30);
+        context.drawImage(IMAGE_CHERRY, this.posX, this.posY, CHERRY_SIZE, CHERRY_SIZE);
     }
 
     update() {
@@ -54,7 +59,7 @@ class Cherry {
     }
 
     collisionCheck() {
-        if(this.posY >= PLAYER_INTIAL_POS_Y - PLAYER_HEIGTH && this.posY >= PLAYER_INTIAL_POS_Y + PLAYER_HEIGTH &&  
+        if(this.posY >= PLAYER_INTIAL_POS_Y - PLAYER_HEIGTH/2 && this.posY <= PLAYER_INTIAL_POS_Y + PLAYER_HEIGTH &&  
             this.posX > pos - PLAYER_WIDTH && this.posX < pos + PLAYER_WIDTH ) {
             this.reset();
             score++;    
@@ -75,17 +80,16 @@ function startTime() {
 }
 
 function timeUpdate() {
-    console.log("updates");
-    console.log(time);
     time += 1;
 }
 
 function timeReset() {
+    clearInterval(timeWatch);
     time = 0;
 }
 
 function timePaused() {
-    time--;
+    time ;
 }
 
 
@@ -101,47 +105,58 @@ function cherryDo(c) {
     c.collisionCheck();
 }
 
-//--------------------------------------------------
-var cherries = new Array(new Cherry);
+function gameReset() {
+    score = 0;
+    difficulty = 1;
+    cherries = new Array(new Cherry);
+}
 
+//--------------------------------------------------
+var cherries;
 
 function mainLoop() {
     requestAnimationFrame(mainLoop);
-    context.clearRect(0,0,canvas.width,canvas.height);
+    context.fillStyle = '#87CEEB'
+    var gr = context.createLinearGradient(0, 500, 0, 500);
+    gr.addColorStop(0, '#87CEEB')
+    gr.addColorStop(1, '#ADD8E6')
+    context.fillRect(0,0,canvas.width,canvas.height);
+    context.stroke();
 
-    context.fillStyle = 'black';
-    context.fillText(score , canvas.width/15, canvas.height/10);
-    context.fillText(3, 14* canvas.width/15, canvas.height/10);
 
     switch(gameState) {
     case GAME_START:
-        context.fillText("BERRY CATCHER", canvas.width/2, canvas.height/2);
-        timePaused();
+        context.drawImage(IMAGE_BC, canvas.width/4, canvas.height/4, canvas.width/2, canvas.height/2);
         break;
     case GAME_PAUSE:
         context.fillText("PAUSED", canvas.width/2, canvas.height/2);
-        timePaused();
         break;
     case GAME_OVER:
-        context.fillText("GAME OVER", canvas.width/2, canvas.height/2);
-        timePaused();
+        context.fillStyle = 'white';
+        context.fillText("SCORE: " + score, canvas.width/2, canvas.height/2);
         break;
     case GAME_RUNNING:
+        context.fillStyle = 'white';
+        context.fillText(score , canvas.width/15, canvas.height/10);
+        context.fillText(100 - time, 14* canvas.width/15, canvas.height/10);
+
         if(time > GAME_TIME) {
             gameState = GAME_OVER;
             timeReset();
         }
-        if(score > difficulty*DIFF_CHANGE) {
+
+        if(difficulty < 10 && score > difficulty*DIFF_CHANGE) {
             difficulty++;
             cherries.push(new Cherry);
         }
+
         cherries.forEach(cherryDo);
-        context.fillStyle = 'yellow';
-        if(pos < 0)
+
+        if(pos + PLAYER_WIDTH < 0)
             pos = canvas.width;
         else if(pos > canvas.width)
-            pos = 0;
-        context.fillRect(pos, PLAYER_INTIAL_POS_Y, PLAYER_WIDTH, PLAYER_HEIGTH);
+            pos = -PLAYER_WIDTH + 1;
+        context.drawImage(IMAGE_BASKET, pos, PLAYER_INTIAL_POS_Y, PLAYER_WIDTH, PLAYER_HEIGTH);
     }
 
 }
@@ -159,7 +174,10 @@ document.addEventListener('keydown', function(e) {
     }
     // Enter
     else if(e.code == "Enter" && gameState > 1) {
-        if(gameState > 2) startTime();
+        if(gameState > 2) {
+            gameReset();
+            startTime();
+        }
         gameState = GAME_RUNNING;
     // 'P'
     } else if(e.code == "KeyP" && gameState === GAME_RUNNING) {
